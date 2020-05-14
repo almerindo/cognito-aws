5c6c54aaf63107c7144748df083a763ad9a72fa1
almerindo <almerindo.rehem@gmailcom>
2020-05-13 22:24:59 -0300

    feat: Add login on cognito

---
+++ src/cognito.config.ts
@@ -0,0 +1,9 @@
+export default {
+  aws_project_region: 'us-east-1',
+  aws_cognito_identity_pool_id:
+    'us-east-1:ea2c0afe-53a5-41e2-85fe-27b558ed9ef4',
+  aws_cognito_region: 'us-east-1',
+  aws_user_pools_id: 'us-east-1_EJnJD6R02',
+  aws_user_pools_web_client_id: '7rr7q54h71uriiocoo159p1v47',
+  oauth: {},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,56 @@
-import s from './error/teste';
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser,
+  AuthenticationDetails,
+} from 'amazon-cognito-identity-js';
 
-console.log('Boilerplate');
-console.log('Boilerplate');
-s('teste');
+import AWS from 'aws-sdk';
+
+import 'isomorphic-fetch';
+
+import config from './cognito.config';
+
+// ##############################################
+const authenticationData = {
+  Username: 'diogo@codengage.com',
+  Password: '123123',
+};
+const authenticationDetails = new AuthenticationDetails(authenticationData);
+const poolData = {
+  UserPoolId: config.aws_user_pools_id, // Your user pool id here
+  ClientId: config.aws_user_pools_web_client_id, // Your client id here
+};
+const userPool = new CognitoUserPool(poolData);
+const userData = {
+  Username: 'diogo@codengage.com', // username
+  Pool: userPool,
+};
+
+const cognitoUser = new CognitoUser(userData);
+
+cognitoUser.authenticateUser(authenticationDetails, {
+  onSuccess(result) {
+    console.log(`access token + ${result.getAccessToken().getJwtToken()}`);
+
+    // POTENTIAL: Region needs to be set if not already set previously elsewhere.
+    AWS.config.region = '<region>';
+
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: '...', // your identity pool id here
+      Logins: {
+        // Change the key below according to the specific region your user pool is in.
+        'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>': result
+          .getIdToken()
+          .getJwtToken(),
+      },
+    });
+
+    // Instantiate aws sdk service objects now that the credentials have been updated.
+    // example: var s3 = new AWS.S3();
+  },
+
+  onFailure(err) {
+    console.error(err);
+  },
+});
